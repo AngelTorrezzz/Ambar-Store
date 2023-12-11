98cf4ce4fd0559cee880300d66bab3e4b9164581
Angel Alvarado <147554100+AngelTorrezzz@users.noreply.github.com>
2023-12-11 02:16:01 -0600

    Update keys.js

--- Server-AmbarStore-Sin-nodemodules/keys.js
+++ Server-AmbarStore-Sin-nodemodules/keys.js
@@ -5,6 +5,7 @@ exports.default = {
         host: 'ls-4916fcb36f2d478a3c98514abb8c023e09bbbd6e.ckpjc07kdumn.us-east-1.rds.amazonaws.com',
         user: 'dbmasteruser',
         password: 'bHU&.%|UwF|Z.[ddPbfG&8xMj7l[J)X(',
+        port: "3000",
         database: 'ambarstore'
     }
 };
